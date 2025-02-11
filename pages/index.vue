6a89568fe848f72eeb3a0558c53485619e4bd90f
Faraz <me.farazzafar@gmail.com>
2025-02-12 00:35:05 +0500

    addition of html and css

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup xmlns="http://www.w3.org/1999/html">
 import {ref, onMounted} from 'vue'
 
 onMounted(() => {
@@ -129,14 +129,226 @@ onMounted(() => {
         </div>
     </section>
     <section class="sell-your-house-with-stress-free-section">
-        <div class="container">
+        <div class="container-fluid">
             <div class="sell-my-house-fast-spokane-wrapper">
                 <div class="house-buyer-team-image">
                     <img src="~assets/images/the-easy-home-buyer-team.jpg">
                 </div>
+                <div class="sell-my-house-fast-spokane-description">
+                    <h1 class="sell-my-house-fast-spokane-heading">Sell Your Spokane House Fast and Stress Free!</h1>
+                    <p class="sell-my-house-fast-spokane-paragraph">The Easy Home Buyer is formed of Chad and Bree
+                        Young, who owned Young’s Quality Cleaning before getting into buying homes. The janitorial
+                        company taught us about the satisfaction of cleaning up dirty places, which was further nurtured
+                        by “Penny.”
+                    </p>
+                    <p class="sell-my-house-fast-spokane-paragraph">
+                        The first-ever home that The Easy Home Buyer purchased was Penny’s, who had to sell her house as
+                        soon as possible before she moved back to Seattle. From Penny’s house, The Easy Home Buyer has
+                        grown by buying numerous Spokane homes.
+                    </p>
+                    <p class="sell-my-house-fast-spokane-paragraph">
+                        As an accredited business, we understand the local market, from the beauty of Manito Park to the
+                        convenience of shopping downtown. We have purchased houses in all corners of the town and
+                        understand the unique attributes of each area.
+                    </p>
+                    <p class="sell-my-house-fast-spokane-paragraph">
+                        Every homeowner who says, “I need to <a href="#"> sell my house fast Spokane</a> ,” can count on
+                        us!
+                    </p>
+                    <div class="sell-my-house-fast-spokane-btn">
+                        <button class="free-offer-header-btn">
+                            GET YOUR CASH OFFER
+                        </button>
+                    </div>
+                </div>
+
             </div>
         </div>
     </section>
+
+    <section class="people-love-us-section">
+        <div class="container-fluid">
+            <div class="people-love-us-wrapper">
+                <h1 class="common-heading how-we-sell-house-fast-heading">
+                    How Do I Sell My House Fast In Spokane?
+                </h1>
+                <p class="how-we-sell-house-fast-paragraph p-0">
+                    See why we have an A+ Rating with the BBB
+                </p>
+            </div>
+            <div class="content-box-container">
+                <div class="people-love-us-content-box-wrapper content-box-wrapper">
+                    <img width="20px" src="~/assets/images/google-icon-logo-svgrepo-com.svg">
+                    <div class="people-love-us-box">
+                        <div class="people-love-us-box-links">
+                            <a class="rating-men-links" href="#">David Fern</a>
+                            <img src="~/assets/images/group-of-stars.png">
+                            <span>March 19, 2023</span>
+                        </div>
+                        <p class="content-paragraph overflow-paragraph">Working with Easy Home Buyer has been a
+                            great
+                            experience. Their
+                            team consistently displays integrity, professionalism, excellent communication, and
+                            really
+                            cares about people and doing things the right way. Throughout our transaction, they were
+                            timely in responding to our needs, helping us deal with a minor curveball, and they have
+                            a
+                            team that is very knowledgeable and experienced. These guys are a great team, and I
+                            highly
+                            recommend working with them!
+                        </p>
+                    </div>
+                </div>
+                <div class="people-love-us-content-box-wrapper content-box-wrapper">
+                    <img width="20px" src="~/assets/images/google-icon-logo-svgrepo-com.svg">
+                    <div class="people-love-us-box">
+                        <div class="people-love-us-box-links">
+                            <a class="rating-men-links" href="#">Ryan Love</a>
+                            <img src="~/assets/images/group-of-stars.png">
+                            <span>March 19, 2023</span>
+                        </div>
+                        <p class="content-paragraph overflow-paragraph">Working with Chad, AJ and the whole team at
+                            The
+                            Easy
+                            Home Buyer has
+                            been a great experience! Every aspect has been extremely smooth..could not ask for a
+                            better
+                            group of people to work with!
+                        </p>
+                    </div>
+                </div>
+                <div class="people-love-us-content-box-wrapper content-box-wrapper">
+                    <img width="20px" src="~/assets/images/google-icon-logo-svgrepo-com.svg">
+                    <div class="people-love-us-box">
+                        <div class="people-love-us-box-links">
+                            <a class="rating-men-links" href="#">Gino</a>
+                            <img src="~/assets/images/group-of-stars.png">
+                            <span>March 19, 2023</span>
+                        </div>
+                        <p class="content-paragraph overflow-paragraph">You now know how the process works, now get
+                            to
+                            know
+                            who we are. We
+                            walk you through the process every step of the way. We are ALWAYS here to help and
+                            answer
+                            questions.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="the-easiest-way-to-sell-house-section">
+        <div class="container-fluid">
+            <div class="how-we-sell-house-fast-wrapper the-easy-way-to-sell-house-wrapper">
+                <h1 class="common-heading how-we-sell-house-fast-heading">
+                    The EASIEST And FASTEST Way
+                </h1>
+                <h1 class="common-heading how-we-sell-house-fast-heading"> To Sell Your Spokane Home</h1>
+                <p class="how-we-sell-house-fast-paragraph" style="padding: 15px 175px 0 175px;">
+                    We have helped hundreds of local sellers, and have NEVER failed to close. Regardless of your
+                    situation we are here to help. This process can be daunting, stressful and loooong. We take all of
+                    that out of the equation to make it the EASIEST, FASTEST most stress-free process GUARANTEED.
+                </p>
+            </div>
+            <div class="easy-way-to-sell-house-points-container">
+                <div class="easy-way-to-sell-house-points-wrapper">
+                    <div class="icon-with-heading easy-way-to-sell-icon-heading">
+                        <img width="60px" src="~assets/images/dollar-money-svgrepo-com.svg">
+                        <h1 class="content-box-heading p-0">Highest Offer in Cash</h1>
+                    </div>
+                    <p class="easy-way-to-sell-house-paragraph">
+                        Every home is more than just walls and windows; its value lies in the life and stories it
+                        houses. The Easy Home Buyer listens to your needs and gives you the best cash offer possible. We
+                        have never failed to close, and it’s an honor to help local sellers get through tough times.
+                    </p>
+                </div>
+                <div class="easy-way-to-sell-house-points-wrapper">
+                    <div class="icon-with-heading easy-way-to-sell-icon-heading">
+                        <img width="60px" src="~assets/images/hamburger-list-menu-svgrepo-com.svg">
+                        <h1 class="content-box-heading p-0">Your Own Conditions</h1>
+                    </div>
+                    <p class="easy-way-to-sell-house-paragraph">
+                        Do you have any last-minute requests, such as donating some of your things to charity or needing
+                        extra time? We’ll be more than happy to accommodate you. The Easy Home Buyer is flexible and
+                        will adjust our offer to your closing time frame and other conditions.
+                    </p>
+                </div>
+                <div class="easy-way-to-sell-house-points-wrapper">
+                    <div class="icon-with-heading easy-way-to-sell-icon-heading">
+                        <img width="60px" src="~assets/images/bank-svgrepo-com%20(1).svg">
+                        <h1 class="content-box-heading p-0">No Bank Processes</h1>
+                    </div>
+                    <p class="easy-way-to-sell-house-paragraph">
+                        We understand how tedious and long bank processes can be, so we take this burden off your
+                        shoulders. The Easy Home Buyer won’t get you into any bank applications. We use cash offers and
+                        handle all the paperwork, so you get a quick sale without hassle.
+                    </p>
+                </div>
+                <div class="easy-way-to-sell-house-points-wrapper">
+                    <div class="icon-with-heading easy-way-to-sell-icon-heading">
+                        <img width="60px" src="~assets/images/tools-wrench-svgrepo-com%20(1).svg">
+                        <h1 class="content-box-heading p-0">No Repairs</h1>
+                    </div>
+                    <p class="easy-way-to-sell-house-paragraph">
+                        The Easy Home Buyer buys houses as is! We understand that you can’t renovate or repair your
+                        Spokane house, so we take care of it in our offer. Regardless of the condition, you’ll still get
+                        a fair offer without hidden fees or commissions.
+                    </p>
+                </div>
+                <div class="easy-way-to-sell-house-points-wrapper">
+                    <div class="icon-with-heading easy-way-to-sell-icon-heading">
+                        <img width="60px" src="~assets/images/cross-svgrepo-com.svg">
+                        <h1 class="content-box-heading p-0">No Out-of-Pocket Costs</h1>
+                    </div>
+                    <p class="easy-way-to-sell-house-paragraph">
+                        Every cash offer is free, so you don’t have to worry about incurring out-of-pocket costs! You
+                        don’t have to prepare for commissions, closing costs, or other fees. Just tell us your Spokane
+                        house’s location and condition, and you’ll get a fair offer right away!
+                    </p>
+                </div>
+                <div class="easy-way-to-sell-house-points-wrapper">
+                    <div class="icon-with-heading easy-way-to-sell-icon-heading">
+                        <img width="60px" src="~assets/images/check-big-svgrepo-com.svg">
+                        <h1 class="content-box-heading p-0">Better Cash Offer?</h1>
+                    </div>
+                    <p class="easy-way-to-sell-house-paragraph">
+                        If you have an offer, let us know! We’ll review and give you a counteroffer if needed. We
+                        understand how important your house is to you, so we ensure that no matter what, you get the
+                        cash offer you deserve.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+
+    </section>
+    <section class="sell-your-house-with-stress-free-section">
+        <div class="container-fluid">
+            <div class="sell-my-house-fast-spokane-wrapper">
+                <div class="house-buyer-team-image">
+                    <img src="~assets/images/cash-home-buyer-spokane.jpg">
+                </div>
+                <div class="sell-my-house-fast-spokane-description">
+                    <h1 class="sell-my-house-fast-spokane-heading">Why We Do What We Do</h1>
+                    <p class="sell-my-house-fast-spokane-paragraph">The Easy Home Buyer believes a house can mean so
+                        much more than just a structure made of wood and concrete. But at the same time, life can be
+                        unpredictable. So, whenever you need to let go of your house quickly and easily, we can help.
+                    </p>
+                    <p class="sell-my-house-fast-spokane-paragraph">
+                        Life can change quickly, and we want to ensure you don’t have to wait months just to get a cash
+                        offer! We want to keep home buying simple, without the hassle of endless paperwork, because
+                        homeowners sell houses for various reasons. Our goal is to give them an easy, stress-free way to
+                        move on.
+                    </p>
+
+
+                </div>
+
+            </div>
+        </div>
+    </section>
+
 </template>
 
 
