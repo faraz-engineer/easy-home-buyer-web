5ce079025ba6556a52cc96339e63809d3618088e
Faraz <me.farazzafar@gmail.com>
2025-02-19 19:22:07 +0500

    addition of html and css

--- components/Header.vue
+++ components/Header.vue
@@ -1,33 +1,35 @@
 <script setup>
 
-import { ref } from 'vue';
+import {ref} from 'vue';
 
 </script>
 
 <template>
     <header>
-          <div class="header-wrapper">
-              <h3 class="common-heading">Spokane & CDA’s Most Trusted Home Buyer</h3>
-              <h3 class="common-heading">Call Us at (509) 530-2088</h3>
-          </div>
-            <div class="header-container">
-                <div class="header-logo">
-                   <a href="#"><img class="img-fluid" src="~assets/images/the-easy-home-buyer-logo.webp"></a>
-                </div>
-                <div class="navbar">
-                    <ul>
-                        <li><a class="navbar-user-link" href="#">About Us</a></li>
-                        <li><a class="navbar-user-link" href="#">FAQ</a></li>
-                        <li><a class="navbar-user-link" href="#">Contact</a></li>
-                    </ul>
-                </div>
-                <div class="header-btn">
-                    <button class="free-offer-header-btn">
-                        GET MY FREE OFFER
-                    </button>
-                </div>
+        <div class="header-wrapper">
+            <h3 class="common-heading">Spokane & CDA’s Most Trusted Home Buyer</h3>
+            <h3 class="common-heading">Call Us at (509) 530-2088</h3>
+        </div>
+        <div class="header-container">
+            <div class="header-logo">
+                <a href="#"><img class="img-fluid" src="~assets/images/the-easy-home-buyer-logo.webp"></a>
             </div>
-
+            <div class="navbar">
+                <ul>
+                    <li><a class="navbar-user-link" href="#">About Us</a></li>
+                    <li><a class="navbar-user-link" href="#">FAQ</a></li>
+                    <li><a class="navbar-user-link" href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="header-btn">
+                <button class="free-offer-header-btn">
+                    GET MY FREE OFFER
+                </button>
+            </div>
+            <div class="hamburger">
+                <img width="32px" src="~/assets/images/hamburger-svgrepo-com.svg">
+            </div>
+        </div>
     </header>
 </template>
 
